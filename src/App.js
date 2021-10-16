bbf7b84ef3e62cf51c2e1902eabcff18163b13d0
VolodymyrTrykoz <volodymyrtz@gmail.com>
2021-10-16 12:39:10 -0400

    code update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My portfolio will be here!
+          My portfolio will be here
         </p>
         <a
           className="App-link"
